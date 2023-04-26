586b229f50742a51e2e4a0f446052e40bfdda4e6
Theo Furtado <theo.furtado@linerasistemas.com.br>
2023-04-26 10:22:10 -0300

    Metodos Getters do cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,4 +1,30 @@
 public class Cliente {
-    int pk_id, idusuario;   
+    int id, idusuario;   
     String nome_completo, email, celular, cpf;
+
+    public int getId(int id){
+        return this.id;
+    }
+
+    public int getUsuarioId(int idusuario){
+        return this.idusuario;
+    }
+
+    public String getNome_Completo(String nome_completo){
+        return this.nome_completo;
+    }
+
+    public String getEmail(String email){
+        return this.email;
+    }
+
+    public String getCelular(String celular){
+        return this.celular;
+    }
+
+    public String getCpf(String cpf){
+        return this.cpf;
+    }
+
+    
 }
--- src/Usuario.java
+++ src/Usuario.java
@@ -3,4 +3,5 @@ public class Usuario {
     String login, senha,perfil_usuario;
 
     
+    
 }
