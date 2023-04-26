2456d4670a2e92d255dd72e62e421ca064a7bbed
Joao Lucas Curvello <103194851+JukasDEV@users.noreply.github.com>
2023-04-26 10:57:15 -0300

    .

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,7 +1,41 @@
 public class Usuario {
-    int pk_id;
-    String login, senha,perfil_usuario;
+   private int Id;
+   private String login, senha,perfil_usuario;
 
+    //Construtor
+    public void AtualizarDadosUsuario(Integer valor_id, String valor_login, String valor_senha, String valor_perfilusuario ){
+        this.Id = valor_id;
+        this.login = valor_login;
+        this.senha = valor_senha;
+        this.perfil_usuario = valor_perfilusuario;
+    }
+
+
+    // Getters
+  public int GetId() {
+    return this.Id;
+  }
+  public String GetLogin(){
+    return this.login;
     
-    
-}
+  }
+  public String GetSenha(){
+    return this.senha;
+  }
+  public String GetPerfilUsuario(){
+    return this.perfil_usuario;
+  }
+  // Setter
+  public void setId(int newId) {
+    this.Id = newId;
+  }
+  public void SetLogin(String newLogin){
+    this.login = newLogin;
+  }
+  public void SetSenha(String newSenha){
+    this.senha = newSenha;
+  }
+  public void SetPerfilUsuario(String newPerfilUsuario){
+    this.perfil_usuario = newPerfilUsuario;
+  }
+}    
