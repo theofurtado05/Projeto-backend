7387038bcc2f812de62b4985ae094b53b84c391a
Theo Furtado <theo.furtado@linerasistemas.com.br>
2023-04-26 10:31:18 -0300

    Metodos Setters do departamento

--- src/Departamento.java
+++ src/Departamento.java
@@ -1,4 +1,20 @@
 public class Departamento {
-    int pk_id;
-    String nome;
+    private int id;
+    private String nome;
+
+    //Getters
+    public int getId(int id){
+        return this.id;
+    }
+    public String getNome(String nome){
+        return this.nome;
+    }
+
+    //Setters
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
 }
