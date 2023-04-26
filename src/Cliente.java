49752ba40614506e695a13305bc0823ef8e22a9a
Theo Furtado <theo.furtado@linerasistemas.com.br>
2023-04-26 10:28:15 -0300

    Metodos Setters do cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,7 +1,8 @@
 public class Cliente {
-    int id, idusuario;   
-    String nome_completo, email, celular, cpf;
+    private int id, idusuario;   
+    private String nome_completo, email, celular, cpf;
 
+    //Getters
     public int getId(int id){
         return this.id;
     }
@@ -25,6 +26,35 @@ public class Cliente {
     public String getCpf(String cpf){
         return this.cpf;
     }
+
+
+    //Setters
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setUsuarioId(int id){
+        this.id = id;
+    }
+
+    public void setNome_completo(String nome_completo){
+        this.nome_completo = nome_completo;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setCelular(String celular){
+        this.celular = celular;
+    }
+
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+
+
+
 
     
 }
