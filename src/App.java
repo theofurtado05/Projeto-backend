5b96ef34e566f96bfb52997fdf352e88c82f6c81
Theo Furtado <theofurtado05@gmail.com>
2023-05-17 11:05:55 -0300

    .

--- src/App.java
+++ src/App.java
@@ -5,11 +5,11 @@ import model.Usuario;
 public class App {
     public static void main(String[] args) throws Exception {
         UsuariosDAO usuarioDao = new UsuariosDAO();
-        Usuario usuario1 = new Usuario(24,"Talita", "Ribeiro", "talita@ibmec.com", PerfilUsuario.ANALISTATI, "abc", 1);
+        Usuario usuario1 = new Usuario(25,"Talita", "Ribeiro", "talit2a@ibmec.com", PerfilUsuario.ANALISTATI, "abc", 1);
         //System.out.println(PerfilUsuario.ANALISTATI.ordinal());
        
 
-        //usuarioDao.create(usuario1);
+        usuarioDao.create(usuario1);
 
     }
 }
