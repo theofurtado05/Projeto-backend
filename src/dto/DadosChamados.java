ed87b2f932ac990e29008751c1f6c743f9b56f0a
Joao Lucas Curvello <103194851+JukasDEV@users.noreply.github.com>
2023-06-05 10:47:35 -0300

    dto

---
+++ src/dto/DadosChamados.java
@@ -0,0 +1,17 @@
+package dto;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+public class DadosChamados {
+    
+
+// Lis = new ArrayList<>();
+
+HashMap<Date, Integer> mapaDatas = new HashMap<>();
+
+
+
+}
